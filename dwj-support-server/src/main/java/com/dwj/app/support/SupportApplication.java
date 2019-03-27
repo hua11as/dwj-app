772971d0e6ed28228d90676186c45b73cf3b7944
as <88452486@qq.com>
2019-03-27 20:35:42 +0800

    init

---
+++ dwj-support-server/src/main/java/com/dwj/app/support/SupportApplication.java
@@ -0,0 +1,16 @@
+package com.dwj.app.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author as
+ * @since 2019/3/27 20:24
+ */
+@SpringBootApplication
+public class SupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportApplication.class, args);
+    }
+}
